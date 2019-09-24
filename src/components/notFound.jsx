070a3b4c76b25c1d15b01a9f682e995679cf1f4a
lokesh-kadiyala98 <33837730+lokesh-kadiyala98@users.noreply.github.com>
2019-09-24 19:48:44 +0530

    'Added component: NotFound'

--- src/components/notFound.jsx
+++ src/components/notFound.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { Container } from 'react-bootstrap';
 
 const NotFound = () => {
   return (
-    <h1>Not Found</h1>
+    <div className="page-notfound">
+      <Container className="page-notfound-text text-center">
+        <h2 className="h1 text-monospace text-light">Uh, Oh! Seems like you are lost.</h2>
+        <h4 className="text-muted">Unfortunately you accessed an invalid page.</h4>
+      </Container>
+    </div>
   );
 };
 

2b68da86dbfa388f859ef752ce09bd2543f744e4
lokesh-kadiyala98 <lokesh.pandu1998@gmail.com>
2020-05-22 18:38:11 +0530

    Feature Added: trainee deatils viewer modal

--- src/components/misc/traineeDetails.jsx
+++ src/components/misc/traineeDetails.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-
-import ReactModal from 'react-modal';
+import { Table } from 'react-bootstrap'
+import ReactModal from 'react-modal'
 
 const TraineeDetails = ({ details, onclick }) => {
-    console.log(details)
+    
     return ( 
         <React.Fragment>
             <ReactModal 
@@ -14,7 +14,122 @@ const TraineeDetails = ({ details, onclick }) => {
                         
                 <button className="close" onClick={onclick}><i className="fas fa-times"></i></button>
                 <div className="p-5">
-                    HI
+                    <Table striped hover>
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Field</th>
+                                <th>Response</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Candidate Name</td>
+                                <td>{details[0].name || 'NA'}</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Individual Identity Number</td>
+                                <td>{details[0].individualIdentityNumber || 'NA'}</td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>Father/ Husband Name</td>
+                                <td>{details[0].fatherOrHusbandName || 'NA'}</td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>Mother Name</td>
+                                <td>{details[0].motherName || 'NA'}</td>
+                            </tr>
+                            <tr>
+                                <td>5</td>
+                                <td>Guardian Name</td>
+                                <td>{details[0].guardianName || 'NA'}</td>
+                            </tr>
+                            <tr>
+                                <td>6</td>
+                                <td>Address</td>
+                                <td>{details[0].address || 'NA'}</td>
+                            </tr>
+                            <tr>
+                                <td>7</td>
+                                <td>Grama</td>
+                                <td>{details[0].grama || 'NA'}</td>
+                            </tr>
+                            <tr>
+                                <td>8</td>
+                                <td>Grama Panchayat</td>
+                                <td>{details[0].gramaPanchayat || 'NA'}</td>
+                            </tr>
+                            <tr>
+                                <td>9</td>
+                                <td>Taluk</td>
+                                <td>{details[0].taluk || 'NA'}</td>
+                            </tr>
+                            <tr>
+                                <td>10</td>
+                                <td>Grama</td>
+                                <td>{details[0].grama || 'NA'}</td>
+                            </tr>
+                            <tr>
+                                <td>11</td>
+                                <td>District</td>
+                                <td>{details[0].district || 'NA'}</td>
+                            </tr>
+                            <tr>
+                                <td>12</td>
+                                <td>Mobile</td>
+                                <td>{details[0].mobile || 'NA'}</td>
+                            </tr>
+                            <tr>
+                                <td>13</td>
+                                <td>Email Id</td>
+                                <td>{details[0].emailId || 'NA'}</td>
+                            </tr>
+                            <tr>
+                                <td>14</td>
+                                <td>Phone</td>
+                                <td>{details[0].phone || 'NA'}</td>
+                            </tr>
+                            <tr>
+                                <td>15</td>
+                                <td>Alternative Mobile</td>
+                                <td>{details[0].alternativeMobile || 'NA'}</td>
+                            </tr>
+                            <tr>
+                                <td>16</td>
+                                <td>Age</td>
+                                <td>{details[0].age || 'NA'}</td>
+                            </tr>
+                            <tr>
+                                <td>17</td>
+                                <td>Education Qualification</td>
+                                <td>{details[0].educationQualification || 'NA'}</td>
+                            </tr>
+                            <tr>
+                                <td>18</td>
+                                <td>Religion</td>
+                                <td>{details[0].religion || 'NA'}</td>
+                            </tr>
+                            <tr>
+                                <td>19</td>
+                                <td>Gender</td>
+                                <td>{details[0].gender || 'NA'}</td>
+                            </tr>
+                            <tr>
+                                <td>20</td>
+                                <td>Category</td>
+                                <td>{details[0].category || 'NA'}</td>
+                            </tr>
+                            <tr>
+                                <td>21</td>
+                                <td>Yearly Income</td>
+                                <td>{details[0].yearlyIncome || 'NA'}</td>
+                            </tr>
+                        </tbody>
+                    </Table>
                 </div>
             </ReactModal>
         </React.Fragment>
